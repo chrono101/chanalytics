f7571200b187b22ef8d66e69e7729dc89dc5e9ef
Cole Cooper <cole@chrono101.com>
2014-10-04 00:39:17 -0400

    Implemented a prototype TF-IDF analytic for replies

--- analytics.js
+++ analytics.js
@@ -352,7 +352,7 @@ var largestImageDimensions = 0;
 var largestImageFs = 0;
 var smallestImageDimensions = 100000000; // Max on /hr/
 var smallestImageFs = 8388608; // Max on /hr/
-var fileNameCounts = [];
+var fileNames = [];
 var fileExtCounts = [];
 var fileSourceCounts = [];
 var fileRatios = [];
@@ -390,7 +390,7 @@ $.each(thread.posts, function (i, post) {
   // If there is an image
   if (post.filename) {
     // Update the filename and file extension counts    
-    fileNameCounts[post.filename + post.ext] = (Number(fileNameCounts[post.filename]) || 0) + 1;
+    fileNames[post.md5] = post;
     fileExtCounts[post.ext] = (Number(fileExtCounts[post.ext]) || 0) + 1;
     // Update the image source counts
     var source = getImageSource(post.filename);
@@ -405,16 +405,16 @@ $.each(thread.posts, function (i, post) {
   }
 });
 
-var nameCountsString = "";
-for(var filename in fileNameCounts) {
-  nameCountsString = nameCountsString + ("<tr><td>" + filename + "</td><td>" + fileNameCounts[filename] + "</td></tr>");
+var namesString = "";
+for(var md5 in fileNames) {
+  namesString = namesString + ("<tr><td>" + generateImageThumb(fileNames[md5]) + "</td><td>" + fileNames[md5].filename + "</td><td>MD5: " + fileNames[md5].md5 + "</td></tr>");
 }
 var extCountsString = "";
 for (var ext in fileExtCounts) {
 extCountsString = extCountsString + ("<tr><td>" + ext + "</td><td>" + fileExtCounts[ext] + "</td></tr>");
 }
 $("#imageStats-FileTypeFreqs").html(extCountsString);
-$("#imageStats-FnFreqs").html(nameCountsString);
+$("#imageStats-FnFreqs").html(namesString);
 
 var sortedFileSourceCounts = [];
 for (var source in fileSourceCounts) {
@@ -458,6 +458,11 @@ function analytic_wordCounts(thread) {
   var wordCounts = [];
   var commonWords = [' ', '', "it's", 'a','able','about','above','abroad','according','accordingly','across','actually','adj','after','afterwards','again','against','ago','ahead','ain\'t','all','allow','allows','almost','alone','along','alongside','already','also','although','always','am','amid','amidst','among','amongst','an','and','another','any','anybody','anyhow','anyone','anything','anyway','anyways','anywhere','apart','appear','appreciate','appropriate','are','aren\'t','around','as','a\'s','aside','ask','asking','associated','at','available','away','awfully','b','back','backward','backwards','be','became','because','become','becomes','becoming','been','before','beforehand','begin','behind','being','believe','below','beside','besides','best','better','between','beyond','both','brief','but','by','c','came','can','cannot','cant','can\'t','caption','cause','causes','certain','certainly','changes','clearly','c\'mon','co','co.','com','come','comes','concerning','consequently','consider','considering','contain','containing','contains','corresponding','could','couldn\'t','course','c\'s','currently','d','dare','daren\'t','definitely','described','despite','did','didn\'t','different','directly','do','does','doesn\'t','doing','done','don\'t','down','downwards','during','e','each','edu','eg','eight','eighty','either','else','elsewhere','end','ending','enough','entirely','especially','et','etc','even','ever','evermore','every','everybody','everyone','everything','everywhere','ex','exactly','example','except','f','fairly','far','farther','few','fewer','fifth','first','five','followed','following','follows','for','forever','former','formerly','forth','forward','found','four','from','further','furthermore','g','get','gets','getting','given','gives','go','goes','going','gone','got','gotten','greetings','h','had','hadn\'t','half','happens','hardly','has','hasn\'t','have','haven\'t','having','he','he\'d','he\'ll','hello','help','hence','her','here','hereafter','hereby','herein','here\'s','hereupon','hers','herself','he\'s','hi','him','himself','his','hither','hopefully','how','howbeit','however','hundred','i','i\'d','ie','if','ignored','i\'ll','i\'m','immediate','in','inasmuch','inc','inc.','indeed','indicate','indicated','indicates','inner','inside','insofar','instead','into','inward','is','isn\'t','it','it\'d','it\'ll','its','it\'s','itself','i\'ve','j','just','k','keep','keeps','kept','know','known','knows','l','last','lately','later','latter','latterly','least','less','lest','let','let\'s','like','liked','likely','likewise','little','look','looking','looks','low','lower','ltd','m','made','mainly','make','makes','many','may','maybe','mayn\'t','me','mean','meantime','meanwhile','merely','might','mightn\'t','mine','minus','miss','more','moreover','most','mostly','mr','mrs','much','must','mustn\'t','my','myself','n','name','namely','nd','near','nearly','necessary','need','needn\'t','needs','neither','never','neverf','neverless','nevertheless','new','next','nine','ninety','no','nobody','non','none','nonetheless','noone','no-one','nor','normally','not','nothing','notwithstanding','novel','now','nowhere','o','obviously','of','off','often','oh','ok','okay','old','on','once','one','ones','one\'s','only','onto','opposite','or','other','others','otherwise','ought','oughtn\'t','our','ours','ourselves','out','outside','over','overall','own','p','particular','particularly','past','per','perhaps','placed','please','plus','possible','presumably','probably','provided','provides','q','que','quite','qv','r','rather','rd','re','really','reasonably','recent','recently','regarding','regardless','regards','relatively','respectively','right','round','s','said','same','saw','say','saying','says','second','secondly','see','seeing','seem','seemed','seeming','seems','seen','self','selves','sensible','sent','serious','seriously','seven','several','shall','shan\'t','she','she\'d','she\'ll','she\'s','should','shouldn\'t','since','six','so','some','somebody','someday','somehow','someone','something','sometime','sometimes','somewhat','somewhere','soon','sorry','specified','specify','specifying','still','sub','such','sup','sure','t','take','taken','taking','tell','tends','th','than','thank','thanks','thanx','that','that\'ll','thats','that\'s','that\'ve','the','their','theirs','them','themselves','then','thence','there','thereafter','thereby','there\'d','therefore','therein','there\'ll','there\'re','theres','there\'s','thereupon','there\'ve','these','they','they\'d','they\'ll','they\'re','they\'ve','thing','things','think','third','thirty','this','thorough','thoroughly','those','though','three','through','throughout','thru','thus','till','to','together','too','took','toward','towards','tried','tries','truly','try','trying','t\'s','twice','two','u','un','under','underneath','undoing','unfortunately','unless','unlike','unlikely','until','unto','up','upon','upwards','us','use','used','useful','uses','using','usually','v','value','various','versus','very','via','viz','vs','w','want','wants','was','wasn\'t','way','we','we\'d','welcome','well','we\'ll','went','were','we\'re','weren\'t','we\'ve','what','whatever','what\'ll','what\'s','what\'ve','when','whence','whenever','where','whereafter','whereas','whereby','wherein','where\'s','whereupon','wherever','whether','which','whichever','while','whilst','whither','who','who\'d','whoever','whole','who\'ll','whom','whomever','who\'s','whose','why','will','willing','wish','with','within','without','wonder','won\'t','would','wouldn\'t','x','y','yes','yet','you','you\'d','you\'ll','your','you\'re','yours','yourself','yourselves','you\'ve','z','zero'];
   var htmlTags = ["br", "a", "span"];
+  
+  var postWordFreq = {}; // { post_number: {term1: count, term2: count}, post_number2:, etc... }
+  var postWordCount = {}; // {post_number: number_of_words, post_number2: number_of_words, etc...}
+  var wordPostCount = {}; // {word: number_of_posts, word2: number_of_posts, etc...}
+  var postTfidf = {}; // {post_number: {term1: tf-idf, term2: tf-idf}, post_number2: {term1: tf-idf}, etc... }
 
 $.each(thread.posts, function (i, post) {
   if (post.com) {
@@ -465,14 +470,58 @@ $.each(thread.posts, function (i, post) {
     post.com = post.com.replace(/[?!.,()]/g,""); // Strip common punctuation
     var words = post.com.split(" ");
 
+    postWordCount[post.no] = words.length; // How many words are in each post?
+    wordFreq = {};
+    
     for (var i = 0; i < words.length; i++) {
       var word = String(words[i].toLowerCase());
       if (commonWords.indexOf(word) == -1) {
         wordCounts[word] = (Number(wordCounts[word]) || 0) + 1;
       }
+      
+      wordFreq[word] = (Number(wordFreq[word]) || 0) + 1;
     }
+    postWordFreq[post.no] = wordFreq;
   }
 });
+
+for (var postNo in postWordFreq) {
+  for (var word in postWordFreq[postNo]) {
+    wordPostCount[word] = (Number(wordPostCount[word]) || 0) + 1;
+  }
+}
+
+// Computes TF-IDF
+for (var postNo in postWordFreq) {
+  wordTfidf = {};
+  
+  for (var word in postWordFreq[postNo]) {
+    var tf = postWordFreq[postNo][word] / postWordCount[postNo];
+    var idf = Math.log(thread.posts.length / wordPostCount[word]);
+    wordTfidf[word] = (tf * idf);
+  
+  }
+  postTfidf[postNo] = wordTfidf;
+}
+
+var tfidfString = "";
+
+for (var postNo in postTfidf) {
+  postTfidfString = "<tr><td>Post No: " + postNo + "</td><td>";
+  sortedTfidf = []  
+  for (var word in postTfidf[postNo]) {    
+      sortedTfidf.push([postNo, word, postTfidf[postNo][word]]);    
+  }
+  sortedTfidf.sort(function (a, b) { return b[2] - a[2]});
+  for (var i = 0; i < sortedTfidf.length; i++) {
+    postTfidfString = postTfidfString + sortedTfidf[i][1] + " ";
+    if (i == 10) break;
+  } 
+  
+  tfidfString = tfidfString + postTfidfString + "</td></tr>";
+}
+
+$("#table-tfidf").html(tfidfString);
 
 var sortedWordCounts = [];
 for (var word in wordCounts) {
@@ -483,8 +532,8 @@ var wordCountsString = "";
 for (var i = 0; i < sortedWordCounts.length; i ++) {
   percentile = (100 * (i - 1) ) / (sortedWordCounts.length - 1);  
   if (percentile > 97) {
-    // Add them in reverse order
     wordCountsString =("<tr><td>" + sortedWordCounts[i][0] + "</td><td>" + sortedWordCounts[i][1] + "</td><td>" + percentile.toPrecision(5) + "</td></tr>") + wordCountsString ;
+    // Add them in reverse order
   }
 }
   wordCountsString = "<thead><tr><td>Word</td><td>Occurances</td><td>Percentile</td></tr></thead>" + wordCountsString;
